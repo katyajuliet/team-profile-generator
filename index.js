f910e4892350d67c51c7afa4a9a56d43dbee643c
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-05-27 13:12:20 -0700

    adding const variables and render fs system for formatting cards and parent to child conent per employee type

---
+++ index.js
@@ -0,0 +1,21 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const distPath = path.join(DIST_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+
+const employees = [];
+
+createTeam ();
+function createTeam() {
+
+
+}
+
