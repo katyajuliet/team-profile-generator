5816b46f791b9ce5e4424535528423f3dd95461c
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-05-27 23:46:21 -0700

    css styling and html render

--- index.js
+++ index.js
@@ -15,7 +15,161 @@ const employees = [];
 
 createTeam ();
 function createTeam() {
+// Inquirer Prompt #1
+console.log("Please build your team")
 
+inquirer.prompt([
+    // Questions about the manager 
+    {
+        type: "input",
+        message: " \n \n What is your manager's name? \n \n ",
+        name: "name",
+        default: "Manager's Name"
+    },
+    {
+        type: "input",
+        message: " \n \n What is your manager's id? \n \n ",
+        name: "id",
+        default: "Manager's ID Number"
+    },
+    {
+        type: "input",
+        message: " \n \n What is your manager's email? \n \n ",
+        name: "email",
+        default: "manager@email.com"
+    },
+    {
+        type: "input",
+        message: " \n \n What is your manager's office number? \n \n ",
+        name: "officeNumber",
+        default: "Manager's Office Number"
+    }
+
+])
+
+    .then(function (managerAnswers) {
+        const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
+        employees.push(manager);
+
+        // Recursion 
+        // Inquirer Prompt #2
+        teamPrompt()
+
+        function teamPrompt() {
+            inquirer.prompt([
+                // Select your team member 
+                {
+                    type: "list",
+                    message: " \n \n Which type of team member would you like to add? \n \n ",
+                    name: "employee",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "No more team members"
+                    ]
+                }
+            ])
+
+                .then(function (typeAnswers) {
+                    if (typeAnswers.employee === "Engineer") {
+
+
+                        // Inquirer Prompt #3
+                        engineerPrompt()
+
+                        function engineerPrompt() {
+                            inquirer.prompt([{
+                                // Questions about the engineer 
+                                type: "input",
+                                message: " \n \n What is your engineer's name? \n \n ",
+                                name: "name",
+                                default: "Engineer's Name"
+                            },
+                            {
+                                type: "input",
+                                message: " \n \n What is your engineer's id? \n \n ",
+                                name: "id",
+                                default: "Engineer's ID Number"
+                            },
+                            {
+                                type: "input",
+                                message: " \n \n What is your engineer's email? \n \n ",
+                                name: "email",
+                                default: "engineer@email.com"
+                            },
+                            {
+                                type: "input",
+                                message: " \n \n What is your engineer's Github? \n \n ",
+                                name: "github",
+                                default: "Engineer's GitHub username"
+                            }
+                            ])
+
+                                .then(function (engineerAnswers) {
+                                    const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github);
+                                    employees.push(engineer);
+                                    teamPrompt();
+                                })
+                        }
+                    } else if (typeAnswers.employee === "Intern") {
+
+                        // Inquirer Prompt #4
+                        internPrompt()
+
+                        function internPrompt() {
+                            inquirer.prompt([
+                                // Questions about the intern
+                                {
+                                    type: "input",
+                                    message: " \n \n What is your intern's name? \n \n ",
+                                    name: "name",
+                                    default: "Intern's Name"
+                                },
+                                {
+                                    type: "input",
+                                    message: " \n \n What is your intern's id? \n \n ",
+                                    name: "id",
+                                    default: "Intern's ID Number"
+                                },
+                                {
+                                    type: "input",
+                                    message: " \n \n What is your intern's email? \n \n ",
+                                    name: "email",
+                                    default: "intern@email.com"
+                                },
+                                {
+                                    type: "input",
+                                    message: " \n \n What is your intern's school? \n \n ",
+                                    name: "school",
+                                    default: "Intern's School"
+                                }
+                            ])
+
+                                .then(function (internAnswers) {
+                                    const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+                                    employees.push(intern);
+                                    teamPrompt();
+                                })
+                        }
+                    } else {
+                        //Exit the inquirer
+                        const html = render(employees);
+                        fs.writeFile(distPath, html, function (err) {
+                            if (err) {
+                                return console.log(err);
+                            };
+
+                        });
+
+                        console.log("Team successfully created!");
+
+
+                    }
+                })
+        }
+    })
 
 }
+
+
 
